dfccb19a805efed37b8abd1cb76eeea47deb1572
Bei <10940770418@qq.com>
2020-03-10 20:41:14 +0800

    添加swagger

--- crm-systemset/crm-systemset-service/src/main/java/com/crm/service/RoleService.java
+++ crm-systemset/crm-systemset-service/src/main/java/com/crm/service/RoleService.java
@@ -1,11 +1,15 @@
 package com.crm.service;
 
 import com.crm.bean.Role;
+import com.crm.bean.RoleExample;
 import com.crm.enums.ExceptionEnums;
 import com.crm.exception.CrmException;
 import com.crm.mapper.RoleMapper;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoleService {
@@ -13,10 +17,21 @@ public class RoleService {
     @Autowired
     private RoleMapper roleMapper;
 
-    public Role findById(Long id){
-        if (id == null){
+    public List<Role> findAll(RoleExample example) {
+        return roleMapper.selectByExample(example);
+    }
+
+    public Role findById(Long id) {
+        if (id == null) {
             throw new CrmException(ExceptionEnums.PARAM_IS_NULL);
         }
         return roleMapper.selectByPrimaryKey(id);
+    }
+
+    public void addRole(Role role) {
+        if (role == null || StringUtils.isBlank(role.getRoleName())) {
+            throw new CrmException(ExceptionEnums.PARAM_IS_NULL);
+        }
+        roleMapper.insert(role);
     }
 }
--- crm-systemset/crm-systemset-service/src/main/java/com/crm/web/controller/RoleController.java
+++ crm-systemset/crm-systemset-service/src/main/java/com/crm/web/controller/RoleController.java
@@ -1,27 +1,45 @@
 package com.crm.web.controller;
 
 import com.crm.bean.Role;
+import com.crm.bean.RoleExample;
 import com.crm.exception.CrmException;
 import com.crm.service.RoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/role")
+@Api(value = "角色管理", tags = {"角色管理"})
 public class RoleController {
 
     @Autowired
     private RoleService roleService;
 
+    @GetMapping("/getAll")
+    public List<Role> getAll(RoleExample example) {
+        return roleService.findAll(example);
+    }
+
+    @PostMapping("/add")
+    public void addRole(Role role) {
+        roleService.addRole(role);
+    }
+
     @GetMapping("/selectById")
-    public ResponseEntity selectById(Long id){
-        try{
+    @ApiOperation("根据id查询角色")
+    public ResponseEntity selectById(Long id) {
+        try {
             Role role = roleService.findById(id);
             return ResponseEntity.ok(role);
-        } catch (CrmException e){
+        } catch (CrmException e) {
             e.printStackTrace();
             return ResponseEntity.status(e.getExceptionEnums().getCode()).body(e.getExceptionEnums().getMsg());
         }
